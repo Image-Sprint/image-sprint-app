6812c1909be1dff3733e19ca5999dd17848dee2f
gt.park <smileboy0014@gmail.com>
2025-07-02 23:10:33 +0900

    feat: errorBoundary 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,34 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  QueryClient,
+  QueryClientProvider,
+  type QueryClientConfig,
+} from '@tanstack/react-query';
+import { API_RETRY_COUNT, API_STALE_TIME } from './constants/api.ts';
+import GlobalErrorBoundary from './pages/Error/GlobalErrorBoundary.tsx';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+      retry: API_RETRY_COUNT,
+      staleTime: API_STALE_TIME,
+      refetchOnWindowFocus: true,
+      refetchOnMount: true,
+    },
+  },
+} as QueryClientConfig);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <GlobalErrorBoundary>
+          <App />
+        </GlobalErrorBoundary>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
