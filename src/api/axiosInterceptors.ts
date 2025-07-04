b7039742c51d8041390610628f064f81969a3e90
gt.park <smileboy0014@gmail.com>
2025-07-04 16:36:28 +0900

    feat: axios 응답 인터셉터 추가 및 API 실패 응답을 예외로 처리

--- src/api/axiosInterceptors.ts
+++ src/api/axiosInterceptors.ts
@@ -1,6 +1,7 @@
 import { getAccessToken } from '@/utils/authUtils';
 import { axiosInstance } from './axiosInstance';
 import { API_PATH } from '@/constants/api';
+import { AxiosError } from 'axios';
 
 const AUTH_EXCLUDE_PATHS = [API_PATH.LOGIN, API_PATH.REFRESH];
 
@@ -13,3 +14,22 @@ axiosInstance.interceptors.request.use((config) => {
 
   return config;
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    const { status, success, message } = response.data;
+
+    if (!success || status !== 200) {
+      const error = new AxiosError(message);
+      error.status = status;
+
+      throw error;
+    }
+
+    return response;
+  },
+  (error) => {
+    // 실제 HTTP 에러 (네트워크 에러, 500 등..)
+    return Promise.reject(error);
+  }
+);
