bfafe7c5ad20b149ebb8b965e8c83e6793e7049a
gt.park <smileboy0014@gmail.com>
2025-07-07 08:55:07 +0900

    feat: 인증 에러 발생 시 에러 페이지로 분기되는 GuardedRoute 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,15 @@ import Profile from './pages/Home/Profile';
 import LoginErrorBoundary from './pages/Error/LoginErrorBoundary';
 import OauthRedirect from './pages/OauthRedirect';
 import '@/api/axiosInterceptors';
+import GuardedRoute from './pages/GuardedRoute';
+import GlobalErrorPage from './pages/Error/GlobalErrorPage';
 
 function App() {
   return (
     <Routes>
+      {/* 공개 라우트 */}
       <Route path="/" element={<Login />} />
+      <Route path={ROUTE_STRING.LOGIN} element={<Login />} />
       <Route
         path={ROUTE_STRING.OAUTH_REDIRECT}
         element={
@@ -22,15 +26,22 @@ function App() {
           </LoginErrorBoundary>
         }
       />
-      <Route path={ROUTE_STRING.LOGIN} element={<Login />} />
-      <Route path={ROUTE_STRING.HOME} element={<Home />}>
-        <Route index element={<Navigate to={ROUTE_STRING.IMAGE} replace />} />
-        <Route path={ROUTE_STRING.IMAGE} element={<Image />} />
-        <Route path={ROUTE_STRING.JOB} element={<Job />} />
-        <Route path={ROUTE_STRING.NOTIFICATION} element={<Notification />} />
-        <Route path={ROUTE_STRING.PROFILE} element={<Profile />} />
+
+      {/* 보호된 라우트 */}
+      <Route element={<GuardedRoute />}>
+        <Route path={ROUTE_STRING.HOME} element={<Home />}>
+          <Route index element={<Navigate to={ROUTE_STRING.IMAGE} replace />} />
+          <Route path={ROUTE_STRING.IMAGE} element={<Image />} />
+          <Route path={ROUTE_STRING.JOB} element={<Job />} />
+          <Route path={ROUTE_STRING.NOTIFICATION} element={<Notification />} />
+          <Route path={ROUTE_STRING.PROFILE} element={<Profile />} />
+        </Route>
       </Route>
 
+      {/* 에러 fallback 전용 라우트 */}
+      <Route path="/error" element={<GlobalErrorPage />} />
+
+      {/* 다른 모든 경로는 홈으로 */}
       <Route
         path="*"
         element={<Navigate to={ROUTE_URL_FULL.IMAGE} replace />}
